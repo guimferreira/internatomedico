2ded3d362e6a0ee277878ac1953435caf5ac0153
Guilherme Medeiros <g_medeiros@outlook.com.br>
2025-05-14 19:03:53 -0300

    Initial commit

---
+++ src/main/java/com/internatomedico/internatomedico/InternatomedicoApplication.java
@@ -0,0 +1,13 @@
+package com.internatomedico.internatomedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternatomedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternatomedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internatomedico/internatomedico/InternatomedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internatomedico.internatomedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternatomedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
